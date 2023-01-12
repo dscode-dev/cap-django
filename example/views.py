df3d5d2aaeee21db4433d0523600b9dac28dd1f1
ds_ <dscode.dsdev@gmail.com>
2023-01-12 11:47:48 -0300

    part2

--- example/views.py
+++ example/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'global/home.html')
+
+def contact(request):
+    return render(request, 'example/contact.html')
+
+def about(request):
+    return render(request, 'example/about.html')
